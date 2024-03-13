e82c10ce65724c986708ee02e0af4d7b9d6f7098
Yiero <958341409@qq.com>
2024-03-13 18:07:36 +0800

    style(0.2.4-b): 1. 添加 GMStorageExtra 类, 用于兼容 ScriptCat 的用户配置, 等待接入

--- src/lib/Storage/GMStorage.ts
+++ src/lib/Storage/GMStorage.ts
@@ -14,13 +14,14 @@ export class GMStorage extends Storage {
 	 * 拒绝用户实例化 GMStorage,
 	 * 只能使用静态方法
 	 * */
+	// eslint-disable-next-line no-useless-constructors
 	private constructor() {
 		super();
 	}
 	
 	/**
 	 * Storage 对象中存储的数据项数量。
-	 * @override
+	 * @override Storage.length
 	 * */
 	static get length(): number {
 		return GM_listValues().length;
@@ -28,7 +29,7 @@ export class GMStorage extends Storage {
 	
 	/**
 	 * 该方法接受一个键名作为参数，返回键名对应的值。
-	 * @override
+	 * @override Storage.getItem()
 	 * */
 	static getItem( key: string, defaultValue?: any ): any | null {
 		return GM_getValue( key, defaultValue );
@@ -43,7 +44,7 @@ export class GMStorage extends Storage {
 	
 	/**
 	 * 该方法接受一个键名和值作为参数，将会把键值对添加到存储中，如果键名存在，则更新其对应的值。
-	 * @override
+	 * @override Storage.setItem()
 	 * */
 	static setItem( key: string, value: string ): void {
 		GM_setValue( key, value );
@@ -51,7 +52,7 @@ export class GMStorage extends Storage {
 	
 	/**
 	 * 该方法接受一个键名作为参数，并把该键名从存储中删除。
-	 * @override
+	 * @override Storage.removeItem()
 	 * */
 	static removeItem( key: string ): void {
 		GM_deleteValue( key );
@@ -59,7 +60,7 @@ export class GMStorage extends Storage {
 	
 	/**
 	 * 调用该方法会清空存储中的所有键名。
-	 * @override
+	 * @override Storage.clear()
 	 * */
 	static clear(): void {
 		const keyList = GM_listValues();
@@ -70,7 +71,7 @@ export class GMStorage extends Storage {
 	
 	/**
 	 * 该方法接受一个数值 n 作为参数，并返回存储中的第 n 个键名。
-	 * @override
+	 * @override Storage.key()
 	 * */
 	static key( index: number ): string | null {
 		return GM_listValues()[ index ];
---
+++ src/lib/Storage/interfaces/index.ts
@@ -0,0 +1,7 @@
+/**
+ * index.ts
+ * created by 2024/3/13
+ * @file 接口入口
+ * @author  Yiero
+ * */
+export * from './ScriptConfigKey.ts';
