c531f02b89caa1b926f2ed7c5ce5895c95a03fa0
Yiero <958341409@qq.com>
2024-03-14 21:11:56 +0800

    style(t-1.1.1): 1. 如果 UserConfigs 为空对象, 则在打包时不输出 yml 字符串

--- build/module/userConfigStringify/userConfigStringify.ts
+++ build/module/userConfigStringify/userConfigStringify.ts
@@ -11,6 +11,13 @@ import YAML from 'yamljs';
  * @tutorial [ScriptCat UserConfig](https://docs.scriptcat.org/docs/dev/config/)
  * */
 export const userConfigStringify = ( userConfig: UserConfigGroupInterface ): string => {
+	/*
+	*  判断用户配置是否为空, 如果为空则返回空字符串
+	* */
+	if (!Boolean(Object.values(userConfig).length)) {
+		return '';
+	}
+	
 	const userinfoYamlString = YAML.stringify( userConfig, 4, 4 );
 	const userinfoHeader = '/* ==UserConfig==';
 	const userinfoFooter = ' ==/UserConfig== */';
